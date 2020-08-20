a1b03526f9604c754212a951a46cc35510f552be
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-08-21 02:39:44 +0900

    First commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$('.carousel__image').square1({
+    // or 'contain'
+    fill_mode: 'cover',
+    // CSS background-position property
+    scale_from: 'center center',
+    // use slide transition
+    animation: 'slide',
+    // in milliseconds
+    slide_duration: 2000,
+    transition_time: 500
+});
