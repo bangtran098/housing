346a1e8b1880ca22739a0cd22a91fac66d8224cf
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-08-22 01:25:40 +0900

    done footer and added animations for component

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $('.carousel__image').square1({
     // or 'contain'
     fill_mode: 'cover',
@@ -10,3 +9,23 @@ $('.carousel__image').square1({
     slide_duration: 2000,
     transition_time: 500
 });
+
+$('.counter').countUp({
+    'time': 1000,
+    'delay': 10
+});
+
+let mainNav = document.getElementById('mainNav');
+let listNav = mainNav.getElementsByTagName("li");
+for (var i = 0; i < listNav.length; i++) {
+    let el = listNav[i];
+    console.log(el);
+    el.addEventListener("click", function() {
+        let current = document.querySelector("#mainNav .active");
+        console.log(current);
+    
+        current.className = current.className.replace("active" , "");
+        // current.className.removeClass("active");
+        this.className += "active";
+    });
+};
